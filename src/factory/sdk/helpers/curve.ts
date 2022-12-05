bdbbb58579d9deacf7268bd455b2380afca6f7d9
Mantas S <mantas487@gmail.com>
2022-12-05 20:20:20 +0200

    curve tested

--- src/factory/sdk/helpers/curve.ts
+++ src/factory/sdk/helpers/curve.ts
@@ -12,20 +12,23 @@ import POOL_ABI from './abi/curvePoolAbi.json';
   Helper Methods
   ==================================================*/
 
-async function getTokens(pools, chain) {
+async function getTokens(pools, block, chain) {
   let poolsTokens = [];
-  pools.forEach((pool) =>
+
+  pools.forEach((pool) => {
     poolsTokens.push(
       util.executeMultiCallsOfTarget(
         pool,
         POOL_ABI,
         ['coins'],
-        Array(8).map((x, i) => i),
-        null,
+        Array(8)
+          .fill(0)
+          .map((x, i) => i),
+        block,
         chain,
       ),
-    ),
-  );
+    );
+  });
   poolsTokens = await Promise.all(poolsTokens);
 
   const poolsWithTokens = {};
@@ -44,26 +47,27 @@ async function getPoolsWithTokens(
   method,
   filePoolCount,
   chainPoolCount,
+  block,
   chain,
 ) {
-  return getTokens(
-    await util
-      .executeMultiCallsOfTarget(
-        curveFactory,
-        FACTORY_ABI,
-        method,
-        Array(parseInt(chainPoolCount) - parseInt(filePoolCount)).map(
-          (x, i) => [filePoolCount + i],
-        ),
-        null,
-        chain,
-      )
-      .then((pools) => pools.map((pool) => pool.toLowerCase())),
-    chain,
-  );
+  const pools = await util
+    .executeMultiCallsOfTarget(
+      curveFactory,
+      FACTORY_ABI,
+      method,
+      Array(parseInt(chainPoolCount) - parseInt(filePoolCount))
+        .fill(0)
+        .map((x, i) => [filePoolCount + i]),
+      block,
+      chain,
+    )
+    .then((pools) => pools.map((pool) => pool.toLowerCase()));
+  const tokens = await getTokens(pools, block, chain);
+
+  return tokens;
 }
 
-async function getPools(curveFactory, chain, provider) {
+async function getPools(curveFactory, block, chain, provider) {
   let pools = {};
   let basePools = {};
   try {
@@ -82,7 +86,7 @@ async function getPools(curveFactory, chain, provider) {
     FACTORY_ABI,
     ['pool_count', 'base_pool_count'],
     [[], []],
-    null,
+    block,
     chain,
   );
 
@@ -95,6 +99,7 @@ async function getPools(curveFactory, chain, provider) {
           'pool_list',
           Object.keys(pools).length,
           poolCount,
+          block,
           chain,
         ),
       ),
@@ -110,6 +115,7 @@ async function getPools(curveFactory, chain, provider) {
           'base_pool_list',
           Object.keys(basePools).length,
           basePoolCount,
+          block,
           chain,
         ),
       ),
@@ -121,6 +127,7 @@ async function getPools(curveFactory, chain, provider) {
       provider,
     );
   }
+
   return {
     ...pools,
     ...basePools,
@@ -134,14 +141,15 @@ async function getPools(curveFactory, chain, provider) {
 async function getTvl(curveFactory, block, chain, provider) {
   const balances = {};
 
-  const pools = await getPools(curveFactory, chain, provider);
-
+  const pools = await getPools(curveFactory, block, chain, provider);
   for (const pool in pools) {
     const poolBalances = await util.executeMultiCallsOfTarget(
       pool,
       POOL_ABI,
       ['balances'],
-      Array(pools[pool].length).map((x, i) => i.toString()),
+      Array(pools[pool].length)
+        .fill(0)
+        .map((x, i) => i.toString()),
       block,
       chain,
     );
@@ -160,6 +168,4 @@ async function getTvl(curveFactory, block, chain, provider) {
   Exports
   ==================================================*/
 
-export default {
-  getTvl,
-};
+export default { getTvl };
