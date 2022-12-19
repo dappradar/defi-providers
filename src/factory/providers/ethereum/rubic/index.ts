8b22c5a48e2beb8da36853de5c0cdfbcf92092d9
Mantas S <mantas487@gmail.com>
2022-12-19 16:19:32 +0200

    rubic (no balances)

---
+++ src/factory/providers/ethereum/rubic/index.ts
@@ -0,0 +1,33 @@
+import util from '../../../../util/blockchainUtil';
+import formatter from '../../../../util/formatter';
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+
+const START_BLOCK = 13905487;
+const POOL = '0xD8b19613723215EF8CC80fC35A1428f8E8826940';
+const TOKENS = [
+  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
+];
+
+async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
+  console.log(params);
+  const { block, chain, web3 } = params;
+  if (block < START_BLOCK) {
+    return {};
+  }
+
+  const tokenBalances = await util.getTokenBalances(
+    POOL,
+    TOKENS,
+    block,
+    chain,
+    web3,
+  );
+
+  const balances = {};
+  formatter.sumMultiBalanceOf(balances, tokenBalances);
+  formatter.convertBalancesToFixed(balances);
+
+  return { balances };
+}
+
+export { tvl };
