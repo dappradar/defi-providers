78d8a9884b68b65b986c992ffbad16c8b36cbecb
osman sonmezturk <osmansonmezturk@gmail.com>
2022-12-27 18:03:18 +0300

    connext is rewrited and nxtp is deleted to fixed npm package error

--- src/factory/providers/moonbeam/connext/index.ts
+++ src/factory/providers/moonbeam/connext/index.ts
@@ -1,16 +1,35 @@
 import BigNumber from 'bignumber.js';
-import { getChainData } from '@connext/nxtp-utils';
-import contractDeployments from '@connext/nxtp-contracts/deployments.json';
 import util from '../../../../util/blockchainUtil';
 import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 import formatter from '../../../../util/formatter';
+import axios from 'axios';
 
 const WGLMR_ADDRESS = '0xacc15dc74880c9944775448304b263d191c6077f';
+let getContractsPromise;
+let getAssetsPromise;
+
+async function getContracts() {
+  if (!getContractsPromise)
+    getContractsPromise = (
+      await axios.get(
+        'https://raw.githubusercontent.com/connext/nxtp/v0.1.40/packages/contracts/deployments.json',
+      )
+    ).data;
+  return getContractsPromise;
+}
+
+async function getAssetIds(chainId) {
+  const url =
+    'https://raw.githubusercontent.com/connext/chaindata/main/crossChain.json';
+  if (!getAssetsPromise) getAssetsPromise = (await axios.get(url)).data;
+  const data = await getAssetsPromise;
+  const chainData = data.find((item) => item.chainId == chainId) || {};
+  return Object.keys(chainData.assetId || {}).map((id) => id.toLowerCase());
+}
 
 async function getDeployedContractAddress(chainId) {
-  const record = contractDeployments[String(chainId)]
-    ? contractDeployments[String(chainId)]
-    : {};
+  const contracts = await getContracts();
+  const record = contracts[String(chainId)] || {};
   const name = Object.keys(record)[0];
   if (!name) {
     return undefined;
@@ -27,15 +46,12 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
 
   const balances = {};
   const contractAddress = await getDeployedContractAddress(1284);
-
-  const chainData = await getChainData();
-
-  const _chain = chainData.get('1284');
+  const assets = await getAssetIds('1284');
   const glmrBalance = await web3.eth.getBalance(contractAddress, block);
 
   const tokenBalances = await util.getTokenBalances(
     contractAddress,
-    Object.keys(_chain.assetId),
+    assets,
     block,
     chain,
     web3,
--- src/factory/providers/moonriver/connext/index.ts
+++ src/factory/providers/moonriver/connext/index.ts
@@ -1,15 +1,33 @@
-import { getChainData } from '@connext/nxtp-utils';
-import contractDeployments from '@connext/nxtp-contracts/deployments.json';
 import util from '../../../../util/blockchainUtil';
 import formatter from '../../../../util/formatter';
 import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+import axios from 'axios';
 
 const START_BLOCK = 863864;
+let getContractsPromise;
+let getAssetsPromise;
+
+async function getContracts() {
+  if (!getContractsPromise)
+    getContractsPromise = (
+      await axios.get(
+        'https://raw.githubusercontent.com/connext/nxtp/v0.1.40/packages/contracts/deployments.json',
+      )
+    ).data;
+  return getContractsPromise;
+}
+async function getAssetIds(chainId) {
+  const url =
+    'https://raw.githubusercontent.com/connext/chaindata/main/crossChain.json';
+  if (!getAssetsPromise) getAssetsPromise = (await axios.get(url)).data;
+  const data = await getAssetsPromise;
+  const chainData = data.find((item) => item.chainId == chainId) || {};
+  return Object.keys(chainData.assetId || {}).map((id) => id.toLowerCase());
+}
 
 async function getDeployedContractAddress(chainId) {
-  const record = contractDeployments[String(chainId)]
-    ? contractDeployments[String(chainId)]
-    : {};
+  const contracts = await getContracts();
+  const record = contracts[String(chainId)] || {};
   const name = Object.keys(record)[0];
   if (!name) {
     return undefined;
@@ -25,14 +43,12 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   }
 
   const balances = {};
-
   const contractAddress = await getDeployedContractAddress(1285);
-  const chainData = await getChainData();
-  const _chain = chainData.get('1285');
+  const assetId = await getAssetIds('1285');
 
   const tokenBalances = await util.getTokenBalances(
     contractAddress,
-    Object.keys(_chain.assetId),
+    assetId,
     block,
     chain,
     web3,
