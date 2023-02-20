477ad365d0c0711933697f7a29a59649a20bc909
osman sonmezturk <osmansonmezturk@gmail.com>
2023-02-20 16:16:00 +0300

    radarstaking is fixed

--- src/factory/providers/bsc/radarstaking/index.ts
+++ src/factory/providers/bsc/radarstaking/index.ts
@@ -4,7 +4,7 @@ import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 
 const START_BLOCK = 19148158;
 const STAKING_REWARDS_PROXY = '0xabdac90a270ef9b6661e3edee922fa051e27593e';
-const STAKING_REWARDS_PROXY_V2 = '0x962B4D8b1f8940D6a67399DC3A8D7549250888B8';
+const STAKING_REWARDS_PROXY_V2 = '0x0C3d5dbc5f785a485a76c8411e5d6D41E0a33850';
 const RADAR_TOKEN = '0x489580eb70a50515296ef31e8179ff3e77e24965';
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
