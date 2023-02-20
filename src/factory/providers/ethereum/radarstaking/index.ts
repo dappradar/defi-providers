dc74e3ebe4bd78af8c24f98c8cd2c4eb5cdb9957
osman sonmezturk <osmansonmezturk@gmail.com>
2023-02-20 16:17:39 +0300

    radarstaking is fixed for ethereum

--- src/factory/providers/ethereum/radarstaking/index.ts
+++ src/factory/providers/ethereum/radarstaking/index.ts
@@ -4,7 +4,7 @@ import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 
 const START_BLOCK = 15051901;
 const STAKING_REWARDS_PROXY = '0x990d156d511d0493a0b7c32d1af97e0c9e352acd';
-const STAKING_REWARDS_PROXY_V2 = '0x0C3d5dbc5f785a485a76c8411e5d6D41E0a33850';
+const STAKING_REWARDS_PROXY_V2 = '0x962B4D8b1f8940D6a67399DC3A8D7549250888B8';
 const RADAR_TOKEN = '0x44709a920fccf795fbc57baa433cc3dd53c44dbe';
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
