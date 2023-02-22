02aa2a1cdd43853927bd115952db896cc509c277
Mantas S <mantas487@gmail.com>
2023-02-22 17:48:49 +0200

    fix

--- src/factory/providers/bsc/pancakeswap/index.ts
+++ src/factory/providers/bsc/pancakeswap/index.ts
@@ -1,63 +1,87 @@
+import { request, gql } from 'graphql-request';
 import BigNumber from 'bignumber.js';
-import * as chef from './chef';
-import * as v1 from './v1';
-import * as v2 from './v2';
 import util from '../../../../util/blockchainUtil';
 import formatter from '../../../../util/formatter';
 import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 
-const POOLS = [
-  '0x73feaa1ee314f8c655e354234017be2193c9e24e',
-  '0x45c54210128a065de780c4b0df3d16664f7f859e',
-  '0x009cf7bc57584b7998236eff51b98a168dcea9b0',
-  '0xa5f8c5dbd5f286960b9d90548680ae5ebff07652',
-  '0xcafeb6f310039968a120d8c095c40a55e85f518c',
-];
-const CAKE_ADDRESS = '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82';
+const START_BLOCK = 600615;
+const THEGRAPTH_ENDPOINT =
+  'https://open-platform.nodereal.io/c12c3655e9f9462db96ae9f61c556011/pancakeswap/graphql';
+const QUERY_SIZE = 1000;
+/*const TOKENS = gql`
+  query getTokens($block: Block_height!,  $skip: Int!) {
+    tokens(block: $block, skip: $skip, first: ${QUERY_SIZE} orderBy: tradeVolumeUSD, orderDirection: desc) {
+      id
+      decimals
+      totalLiquidity
+    }
+  }
+`;*/
+const TOKENS = gql`
+  query getTokens($skip: Int!) {
+    tokens(skip: $skip, first: ${QUERY_SIZE} orderBy: tradeVolumeUSD, orderDirection: desc) {
+      id
+      decimals
+      totalLiquidity
+    }
+  }
+`;
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
-  const { block, date, chain, provider, web3 } = params;
+  const { block, chain, provider, web3 } = params;
 
-  if (block < 600615) {
+  if (block < START_BLOCK) {
     return {};
   }
   const balances = {};
 
-  const cakeBalances = await util.getTokenBalancesOfHolders(
-    POOLS,
-    Array(POOLS.length).fill(CAKE_ADDRESS),
+  let skip = 0;
+
+  // 2000 is max skip
+  while (skip <= 2000) {
+    const requestResult = await request(THEGRAPTH_ENDPOINT, TOKENS, {
+      block: block,
+      skip: skip,
+    });
+
+    for (const token of requestResult.tokens) {
+      if (
+        token.id.toLowerCase() !== '0xc61713cd43ccb88ef0aae42281cc51f9ed974b78' //scam token
+      ) {
+        balances[token.id.toLowerCase()] = BigNumber(
+          token.totalLiquidity,
+        ).shiftedBy(Number(token.decimals));
+      }
+    }
+
+    if (requestResult.tokens.length < QUERY_SIZE) {
+      break;
+    }
+    skip += QUERY_SIZE;
+  }
+
+  const stableSwapBalances = await util.getTokenBalancesOfEachHolder(
+    [
+      '0x169f653a54acd441ab34b73da9946e2c451787ef',
+      '0xc2f5b9a3d9138ab2b74d581fc11346219ebf43fe',
+      '0x3efebc418efb585248a0d2140cfb87afcc2c63dd',
+      '0x6d8fba276ec6f1eda2344da48565adbca7e4ffa5',
+      '0x49079d07ef47449af808a4f36c2a8dec975594ec',
+    ],
+    [
+      '0x55d398326f99059ff775485246999027b3197955', //USDT
+      '0xe9e7cea3dedca5984780bafc599bd69add087d56', //BUSD
+      '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d', //USDC
+      '0x4268b8f0b87b6eae5d897996e6b845ddbd99adf3', //AXLUSDC
+      '0x0782b6d8c4551b9760e74c0545a9bcd90bdc41e5', //HAY
+    ],
     block,
     chain,
     web3,
   );
+  formatter.sumMultiBalanceOf(balances, stableSwapBalances);
 
-  formatter.sumMultiBalanceOf(balances, cakeBalances, chain, provider);
-
-  const results = await Promise.all([
-    chef.getBalances(block, chain, provider, web3),
-    // v1.getBalances(block, date, chain, provider, web3),
-    v2.getBalances(block, date, chain, provider, web3),
-  ]);
-
-  console.log(results.length);
-  results.forEach((result) => {
-    if (result) {
-      for (const token in result) {
-        if (!balances[token]) {
-          balances[token] = BigNumber(0);
-        }
-        balances[token] = balances[token].plus(result[token]);
-      }
-    }
-  });
-
-  for (const token in balances) {
-    if (balances[token].isLessThan(1000000)) {
-      delete balances[token];
-    } else {
-      balances[token] = balances[token].toFixed();
-    }
-  }
+  formatter.convertBalancesToFixed(balances);
 
   return { balances };
 }
