9e135449590731454e2931f44bdc51fa88ecbc48
osman sonmezturk <osmansonmezturk@gmail.com>
2023-03-23 13:38:44 +0300

    Bsc uniswapv3 is integrated

---
+++ src/factory/providers/bsc/uniswapv3/index.ts
@@ -0,0 +1,23 @@
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+import formatter from '../../../../util/formatter';
+import uniswapV3 from '../../../../util/calculators/uniswapV3';
+
+const START_BLOCK = 0;
+const THEGRAPTH_ENDPOINT =
+  'https://api.thegraph.com/subgraphs/name/revert-finance/uniswap-v3-bnb';
+
+async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
+  const { block, chain } = params;
+  if (block < START_BLOCK) {
+    return {};
+  }
+
+  const balances = await uniswapV3.getTvlFromSubgraph(
+    THEGRAPTH_ENDPOINT,
+    block,
+    chain,
+  );
+  formatter.convertBalancesToFixed(balances);
+  return { balances };
+}
+export { tvl };
