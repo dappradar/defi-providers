5b1a1f948d960fc6050e4aaaa6d6f43dce3a4358
Mantas S <mantas487@gmail.com>
2023-04-26 17:00:46 +0300

    new contracts are included

--- src/factory/providers/bsc/radarstaking/index.ts
+++ src/factory/providers/bsc/radarstaking/index.ts
@@ -4,7 +4,8 @@ import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 
 const START_BLOCK = 19148158;
 const STAKING_REWARDS_PROXY = '0xabdac90a270ef9b6661e3edee922fa051e27593e';
-const STAKING_REWARDS_PROXY_V2 = '0x0C3d5dbc5f785a485a76c8411e5d6D41E0a33850';
+const STAKING_REWARDS_PROXY_V2 = '0x0c3d5dbc5f785a485a76c8411e5d6d41e0a33850';
+const RADAR_STAKE_V3 = '0x718bad5717932fd30bf37804332c4db69c840a8e';
 const RADAR_TOKEN = '0x489580eb70a50515296ef31e8179ff3e77e24965';
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
@@ -14,7 +15,7 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   }
 
   const proxyBalance = await util.getTokenBalancesOfEachHolder(
-    [STAKING_REWARDS_PROXY, STAKING_REWARDS_PROXY_V2],
+    [STAKING_REWARDS_PROXY, STAKING_REWARDS_PROXY_V2, RADAR_STAKE_V3],
     [RADAR_TOKEN],
     block,
     chain,
