eebabc406012e244d4b0838f1a0c430f8cb8373f
Mantas S <mantas487@gmail.com>
2023-06-27 15:23:35 +0300

    interport integration

---
+++ src/factory/providers/zksync-era/interport/index.ts
@@ -0,0 +1,40 @@
+import util from '../../../../util/blockchainUtil';
+import formatter from '../../../../util/formatter';
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+import VAULT_ABI from './vaultAbi.json';
+
+const START_BLOCK = 0;
+const VAULT_ADDRESS = '0xc724832c5ed81599ae3e4ebc0ec4f87a285b5838';
+
+async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
+  const { block, chain, provider, web3 } = params;
+  const balances = {};
+  if (block < START_BLOCK) {
+    return { balances };
+  }
+
+  const assetAddress = await util.executeCall(
+    VAULT_ADDRESS,
+    VAULT_ABI,
+    'asset',
+    [],
+    block,
+    chain,
+    web3,
+  );
+
+  const vaultBalance = await util.getTokenBalances(
+    VAULT_ADDRESS,
+    [assetAddress],
+    block,
+    chain,
+    web3,
+  );
+
+  formatter.sumMultiBalanceOf(balances, vaultBalance);
+  formatter.convertBalancesToFixed(balances);
+
+  return { balances };
+}
+
+export { tvl };
