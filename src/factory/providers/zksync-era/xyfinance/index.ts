1b504071c772a9c75244b8c36fde8960a11f29b8
Mantas S <mantas487@gmail.com>
2023-06-27 17:05:32 +0300

    xyfinance integration

--- src/factory/providers/zksync-era/xyfinance/index.ts
+++ src/factory/providers/zksync-era/xyfinance/index.ts
@@ -1,10 +1,11 @@
 import util from '../../../../util/blockchainUtil';
 import formatter from '../../../../util/formatter';
 import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
-import VAULT_ABI from './vaultAbi.json';
 
-const START_BLOCK = 3242317;
-const VAULT_ADDRESS = '0xc724832c5ed81599ae3e4ebc0ec4f87a285b5838';
+const START_BLOCK = 2210653;
+const ETH_HOLDER_ADDRESS = '0x935283a00fbf8e40fd2f8c432a488f6addc8db67';
+const USDC_HOLDER_ADDRESS = '0x75167284361c8d61be7e4402f4953e2b112233cb';
+const USDC_ADDRESS = '0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4';
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   const { block, chain, provider, web3 } = params;
@@ -13,25 +14,23 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
     return { balances };
   }
 
-  const assetAddress = await util.executeCall(
-    VAULT_ADDRESS,
-    VAULT_ABI,
-    'asset',
-    [],
+  const usdcBalance = await util.getTokenBalancesOfHolders(
+    [USDC_HOLDER_ADDRESS],
+    [USDC_ADDRESS],
     block,
     chain,
     web3,
   );
 
-  const vaultBalance = await util.getTokenBalances(
-    VAULT_ADDRESS,
-    [assetAddress],
+  const ethBalance = await util.getBalancesOfHolders(
+    [ETH_HOLDER_ADDRESS],
     block,
     chain,
     web3,
   );
 
-  formatter.sumMultiBalanceOf(balances, vaultBalance);
+  formatter.sumMultiBalanceOf(balances, usdcBalance);
+  formatter.sumMultiBalanceOf(balances, ethBalance);
   formatter.convertBalancesToFixed(balances);
 
   return { balances };
