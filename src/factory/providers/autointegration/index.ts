a6923b752eb7356ee6c0cf9a8b2e058ac1c5d869
Mantas S <mantas487@gmail.com>
2023-07-28 09:53:56 +0300

    final

--- src/factory/providers/autointegration/index.ts
+++ src/factory/providers/autointegration/index.ts
@@ -1,5 +1,5 @@
 import { AbiItem } from 'web3-utils';
-import uniswapV2 from '../../../util/calculators/uniswapV2';
+import uniswapV2 from './uniswapV2';
 import formatter from '../../../util/formatter';
 import BigNumber from 'bignumber.js';
 import {
@@ -26,46 +26,33 @@ async function tvl(
     const result = { balances: {}, poolBalances: {} };
 
     for (const address of addresses.split(',')) {
-      console.log('address:', address);
-      if (
-        address !== '0x0625468f8F56995Ff1C27EB6FD44ac90E96C5D22' &&
-        address !== '0x8CB1fEE69f7F8f00efd5d47067eb75C19cd40017'
-      ) {
-        const contract = new web3.eth.Contract(
-          FACTORY_ABI as AbiItem[],
-          address,
-        );
+      const contract = new web3.eth.Contract(FACTORY_ABI as AbiItem[], address);
 
-        let usePoolMethodsFlg = false;
-        try {
-          await contract.methods.allPairsLength().call(null, block);
-        } catch {
-          usePoolMethodsFlg = true;
-        }
-
-        const { balances, poolBalances } = await uniswapV2.getTvl(
-          address,
-          block,
-          chain,
-          provider,
-          web3,
-          usePoolMethodsFlg,
-        );
-
-        for (const token in balances) {
-          result.balances[token] = BigNumber(result.balances[token] || 0).plus(
-            balances[token],
-          );
-        }
-        Object.assign(result.poolBalances, poolBalances);
-
-        console.log('balances', formatter.convertBalancesToFixed(balances));
-        console.log('poolBalances', poolBalances);
+      let usePoolMethodsFlg = false;
+      try {
+        await contract.methods.allPairsLength().call(null, block);
+      } catch {
+        usePoolMethodsFlg = true;
       }
+
+      const { balances, poolBalances } = await uniswapV2.getTvl(
+        address,
+        block,
+        chain,
+        provider,
+        web3,
+        usePoolMethodsFlg,
+      );
+
+      for (const token in balances) {
+        result.balances[token] = BigNumber(result.balances[token] || 0).plus(
+          balances[token],
+        );
+      }
+      Object.assign(result.poolBalances, poolBalances);
     }
 
     formatter.convertBalancesToFixed(result.balances);
-    console.log('result', result);
     return result;
   }
 }
