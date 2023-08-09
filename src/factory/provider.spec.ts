548ca19c366439f232312e3f21289e7d4df3b1b0
Mantas S <mantas487@gmail.com>
2023-08-09 12:50:30 +0300

    Update provider.spec.ts

--- src/factory/provider.spec.ts
+++ src/factory/provider.spec.ts
@@ -30,11 +30,6 @@ describe(process.argv[6], () => {
         chain,
         block,
         date: '0',
-        autointegrationParams: {
-          autointegrated: 'false',
-          dappType: '',
-          addresses: '',
-        },
       });
       console.log('balances:', balances);
       if (showPoolBalancesFlg) console.log('poolBalances:', poolBalances);
