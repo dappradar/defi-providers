8ebffe151a307a69949a48ee46efea0c10fd91c2
Mantas S <mantas487@gmail.com>
2023-08-17 16:48:46 +0300

    crvUsd included into TVL

--- src/factory/providers/ethereum/curve/index.ts
+++ src/factory/providers/ethereum/curve/index.ts
@@ -1,9 +1,11 @@
 import BigNumber from 'bignumber.js';
 import abi from './abi.json';
+import crvUsdPoolAbi from './crvUsdPoolAbi.json';
 import formatter from '../../../../util/formatter';
 import util from '../../../../util/blockchainUtil';
 import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 import curve from '../../../../util/calculators/curve';
+import basicUtil from '../../../../util/basicUtil';
 
 const CURVE_ADDRESS_PROVIDER = '0x0000000022d53366457f9d5e68ec105046fc4383';
 const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
@@ -57,12 +59,86 @@ const CURVE_POOLS = [
   '0xDeBF20617708857ebe4F679508E7b7863a8A8EeE',
 ];
 
+const CRV_USD_POOL_FACTORIES = [
+  {
+    factory: '0x818709b85052ddc521fae9c78737b27316337e3a',
+    startBlock: 17182152,
+  },
+  {
+    factory: '0x30a2F3c3AA6D12C0a36Bed210dCF1B32EF6228Cc',
+    startBlock: 17187330,
+  },
+  {
+    factory: '0xfa3e2db8eb6c646e0d24046c1a185934d41a8f7a',
+    startBlock: 17187330,
+  },
+  {
+    factory: '0xC9332fdCB1C491Dcc683bAe86Fe3cb70360738BC',
+    startBlock: 17187330,
+  },
+];
+
 function convertToBaseToken(address) {
   const token = address.toLowerCase();
   if (!BASE_TOKENS[token]) {
     return token;
   }
   return BASE_TOKENS[token];
+}
+
+async function getCrvUsdPoolsTokenBalances(block, chain, provider, web3) {
+  const blocksLimit = 10000;
+  const poolLogs = [];
+
+  let data = { lastScannedblock: 0, holders: [], tokens: [] };
+  try {
+    data = await basicUtil.readFromCache('crvUsdData.json', chain, provider);
+  } catch {}
+
+  for (let i = 0, l = CRV_USD_POOL_FACTORIES.length; i < l; i++) {
+    if (CRV_USD_POOL_FACTORIES[i].startBlock < block) {
+      for (
+        let j = Math.max(
+          CRV_USD_POOL_FACTORIES[i].startBlock,
+          data.lastScannedblock,
+        );
+        j < block;
+        j += blocksLimit
+      ) {
+        const logs = await util.getDecodedLogsParameters(
+          j,
+          Math.min(j + blocksLimit, block),
+          '0xebbe0dfde9dde641808b7a803882653420f3a5b12bb405d238faed959e1e3aa3',
+          CRV_USD_POOL_FACTORIES[i].factory,
+          'collateral',
+          [
+            { type: 'address', name: 'controller' },
+            { type: 'address', name: 'amm' },
+            { type: 'address', name: 'monetary_policy' },
+            { type: 'uint256', name: 'ix' },
+          ],
+          web3,
+        );
+
+        Array.prototype.push.apply(poolLogs, logs);
+        logs.forEach((decodedLogParameters) => {
+          data.holders.push(decodedLogParameters.amm.toLowerCase());
+          data.tokens.push(decodedLogParameters.collateral.toLowerCase());
+        });
+      }
+    }
+  }
+
+  data.lastScannedblock = block;
+  await basicUtil.saveIntoCache(data, 'crvUsdData.json', chain, provider);
+
+  return await util.getTokenBalancesOfHolders(
+    data.holders,
+    data.tokens,
+    block,
+    chain,
+    web3,
+  );
 }
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
@@ -221,6 +297,16 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   delete balances['0x6c3f90f043a72fa612cbac8115ee7e52bde6e490'];
   delete balances['0x075b1bb99792c9e1041ba13afef80c91a1e70fb3'];
   delete balances['0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8'];
+
+  const crvUsdPoolsTokenBalances = await getCrvUsdPoolsTokenBalances(
+    block,
+    chain,
+    provider,
+    web3,
+  );
+  formatter.sumMultiBalanceOf(balances, crvUsdPoolsTokenBalances);
+  formatter.convertBalancesToFixed(balances);
+
   return { balances };
 }
 
