d97692834d76a8761420642f19aa3e1ddd8facb5
Mantas S <mantas487@gmail.com>
2023-09-18 16:01:37 +0300

    gUni

--- src/factory/providers/ethereum/makerdao/index.ts
+++ src/factory/providers/ethereum/makerdao/index.ts
@@ -7,19 +7,20 @@ import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 import ERC20_ABI from '../../../../constants/abi/erc20.json';
 import CDP_MANAGER_ABI from './abi/cdpManager.json';
 import IKL_REGISTRY_ABI from './abi/ilkRegistry.json';
-import MCD_MONITOR_V2 from './abi/mcdMonitorV2.json';
+import MCD_MONITOR_V2_ABI from './abi/mcdMonitorV2.json';
+import G_UNI_ABI from './abi/gUni.json';
 
 const START_BLOCK = 14583413;
 const BLOCK_LIMIT = 10000;
 
-// TO DO MOVE TO STORAGE
-const API_URL = '';
+const API_URL = process.env.SUMMERFI_CONFIRMED_VAULTS_API;
 const CONTRACTS = {
   AutomationV1Contract: '0x6E87a7A0A03E51A741075fDf4D1FCce39a4Df01b',
   McdMonitorV2: '0xa59d5E94BFE605A9a4aC7e02f5380e02061c8dd2',
   CdpManager: '0x5ef30b9986345249bc32d8928b7ee64de9435e39',
   IlkRegistry: '0x5a464c28d19848f44199d003bef5ecc87d090f87',
 };
+const G_UNI_DAI_USDC_ADDRESS = '0x50379f632ca68d36e50cfbc8f78fe16bd1499d1e';
 
 const LOGS_TOPIC = {
   TriggerAdded:
@@ -47,7 +48,6 @@ async function getAutomationCdpIdList(block, chain, provider, web3) {
     i < block;
     i += BLOCK_LIMIT
   ) {
-    console.log('loop', i);
     const [triggerAddedLogs, triggerRemovedLogs, triggerExecutedLogs] =
       await Promise.all(
         Object.keys(LOGS_TOPIC).map((key) =>
@@ -114,6 +114,91 @@ async function getAutomationCdpIdList(block, chain, provider, web3) {
   await basicUtil.saveIntoCache(cache, 'cache.json', chain, provider);
 
   return cdpIdList;
+}
+
+async function automationTvl(cdpIdList, balances, block, chain, web3) {
+  const cdpIds = [...cdpIdList];
+
+  const ilkNames = await util.executeMultiCallsOfTarget(
+    CONTRACTS.CdpManager,
+    CDP_MANAGER_ABI,
+    'ilks',
+    cdpIds,
+    block,
+    chain,
+    web3,
+  );
+
+  const cdpIlkIds = {};
+  ilkNames.forEach((val, idx) => (cdpIlkIds[cdpIds[idx]] = val));
+  const ilkIds = [...new Set(ilkNames)];
+  const tokens = (
+    await util.executeMultiCallsOfTarget(
+      CONTRACTS.IlkRegistry,
+      IKL_REGISTRY_ABI,
+      'info',
+      ilkIds,
+      block,
+      chain,
+      web3,
+    )
+  ).map((i) => i[4]);
+  const decimals = await util.executeCallOfMultiTargets(
+    tokens,
+    ERC20_ABI,
+    'decimals',
+    [],
+    block,
+    chain,
+    web3,
+  );
+
+  const collData = await util.executeMultiCallsOfTarget(
+    CONTRACTS.McdMonitorV2,
+    MCD_MONITOR_V2_ABI,
+    'getCdpInfo',
+    cdpIds.map((i) => [i, cdpIlkIds[i]]),
+    block,
+    chain,
+    web3,
+  );
+  collData.forEach((coll, i) => {
+    const idx = ilkIds.indexOf(ilkNames[i]);
+    balances[String(tokens[idx]).toLowerCase()] = BigNumber(
+      balances[tokens[idx].toLowerCase()] || 0,
+    ).plus(coll['0'] / 10 ** (18 - decimals[idx]));
+  });
+}
+
+async function convertGUniToUnderlyings(balances, block, chain, web3) {
+  if (balances[G_UNI_DAI_USDC_ADDRESS]) {
+    const [totalSupply, underlyingBalances, token0, token1] = await Promise.all(
+      await util.executeDifferentCallsOfTarget(
+        G_UNI_DAI_USDC_ADDRESS,
+        G_UNI_ABI,
+        ['totalSupply', 'getUnderlyingBalances', 'token0', 'token1'],
+        [[], [], [], []],
+        block,
+        chain,
+        web3,
+      ),
+    );
+
+    const ratio = new BigNumber(balances[G_UNI_DAI_USDC_ADDRESS]).dividedBy(
+      totalSupply,
+    );
+    balances[token0.toLowerCase()] = BigNumber(
+      balances[token0.toLowerCase()] || 0,
+    ).plus(
+      new BigNumber(underlyingBalances.amount0Current).multipliedBy(ratio),
+    );
+    balances[token1.toLowerCase()] = BigNumber(
+      balances[token1.toLowerCase()] || 0,
+    ).plus(
+      new BigNumber(underlyingBalances.amount1Current).multipliedBy(ratio),
+    );
+    delete balances[G_UNI_DAI_USDC_ADDRESS];
+  }
 }
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
@@ -185,7 +270,7 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
 
   const collData = await util.executeMultiCallsOfTarget(
     CONTRACTS.McdMonitorV2,
-    MCD_MONITOR_V2,
+    MCD_MONITOR_V2_ABI,
     'getCdpInfo',
     filteredVaultsList.map((i) => [i, cdpIlkIds[i]]),
     block,
@@ -201,6 +286,10 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
       balances[tokens[idx].toLowerCase()] || 0,
     ).plus(coll['0'] / 10 ** (18 - decimals[idx]));
   });
+
+  await automationTvl(cdpIdList, balances, block, chain, web3);
+
+  await convertGUniToUnderlyings(balances, block, chain, web3);
 
   const tokenBalances = {};
   Object.keys(balances).forEach(function (key) {
