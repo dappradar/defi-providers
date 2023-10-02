cbaa6793a6f28199f0a1629a17e36a7f586467a0
Mantas S <mantas487@gmail.com>
2023-10-02 19:24:21 +0300

    Update index.ts

--- src/factory/providers/ethereum/instadapp/index.ts
+++ src/factory/providers/ethereum/instadapp/index.ts
@@ -1,6 +1,7 @@
 import BigNumber from 'bignumber.js';
 import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 import fetch from 'node-fetch';
+import { log } from '../../../../util/logger/logger';
 
 const API =
   'https://api.internal.instadapp.io/defi/api/stats/instadapp/overall';
@@ -13,6 +14,11 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
 
   const balances = {};
 
+  const response = await fetch(API);
+  log.info({
+    message: await response.text(),
+    endpoint: 'instadapp.tvl',
+  });
   balances['0xdac17f958d2ee523a2206206994597c13d831ec7'] = await fetch(API)
     .then((res) => res.json())
     .then((res) => BigNumber(res.stats[0].totalSupplied).shiftedBy(6));
