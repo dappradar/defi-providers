34ee64b9c65a0396846f13ab6b5178bf9d965f12
Mantas S <mantas487@gmail.com>
2023-11-23 15:46:28 +0200

    syncswap, metavault

---
+++ src/factory/providers/linea/metavault/index.ts
@@ -0,0 +1,25 @@
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+import uniswapV3 from '../../../../util/calculators/uniswapV3chain';
+
+const V3_START_BLOCK = 652486;
+const V3_FACTORY_ADDRESS = '0x9367c561915f9D062aFE3b57B18e30dEC62b8488';
+
+async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
+  const { block, chain, provider, web3 } = params;
+  if (block < V3_START_BLOCK) {
+    return { balances: {} };
+  }
+
+  const balances = await uniswapV3.getTvl(
+    V3_FACTORY_ADDRESS,
+    V3_START_BLOCK,
+    block,
+    chain,
+    provider,
+    web3,
+  );
+
+  return { balances, poolBalances: {} };
+}
+
+export { tvl };
---
+++ src/util/calculators/uniswapV3chain.ts
@@ -0,0 +1,164 @@
+import formatter from '../../util/formatter';
+import basicUtil from '../../util/basicUtil';
+import util from '../../util/blockchainUtil';
+import { log } from '../../util/logger/logger';
+import PAIR_ABI from '../../constants/abi/uni.json';
+import { IBalances } from '../../interfaces/ITvl';
+import Web3 from 'web3';
+
+/**
+ * Gets TVL of Uniswap V2 (or it's clone) using factory address
+ *
+ * @param factoryAddress - The address of factory
+ * @param startBlock - The block number of factory deploying transaction
+ * @param block - The block number for which data is requested
+ * @param chain - EVM chain name (providers parent folder name)
+ * @param provider - the provider folder name
+ * @param web3 - The Web3 object
+ * @returns The object containing object with token addresses and their locked values and object with pool balances
+ *
+ */
+async function getTvl(
+  factoryAddress: string,
+  startBlock: number,
+  block: number,
+  chain: string,
+  provider: string,
+  web3: Web3,
+): Promise<IBalances> {
+  const balances = {};
+
+  let v3Pairs = { block: startBlock, pairs: [], token01: [] };
+  try {
+    v3Pairs = await basicUtil.readFromCache(
+      'cache/v3Pairs.json',
+      chain,
+      provider,
+    );
+  } catch {}
+
+  let logs = [];
+  console.log('[v3] start getting tvl');
+  console.log('[v3] get logs1');
+
+  let offset = 10000;
+  for (let i = Math.max(v3Pairs.block, startBlock); ; ) {
+    console.log(`Trying from ${i} with offset ${offset}`);
+    let eventLog = [];
+    try {
+      eventLog = (
+        await util.getLogs(
+          i,
+          Math.min(block, i + offset),
+          '0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118',
+          factoryAddress,
+          web3,
+        )
+      ).output;
+      console.log(`Trying from ${i} with offset ${offset}`);
+    } catch (e) {
+      log.error({
+        message: e?.message || '',
+        stack: e?.stack || '',
+        detail: `Error: tvl of ethereum/uniswapv3`,
+        endpoint: 'tvl',
+      });
+      if (offset > 1000) {
+        offset -= 2000;
+      } else if (offset > 100) {
+        offset -= 200;
+      } else if (offset > 10) {
+        offset -= 20;
+      } else {
+        break;
+      }
+      continue;
+    }
+    logs = logs.concat(eventLog);
+
+    i += offset;
+    if (block < i) {
+      break;
+    }
+  }
+
+  const pairs = v3Pairs.token01;
+  let pairAddresses = v3Pairs.pairs;
+  const start = 128 - 40 + 2;
+  const end = 128 + 2;
+
+  const pairExist = {};
+  pairAddresses.forEach((address) => (pairExist[address] = true));
+
+  pairAddresses = pairAddresses.concat(
+    logs
+      .map((log) => {
+        let pairAddress = `0x${log.data.slice(start, end)}`;
+        pairAddress = pairAddress.toLowerCase();
+
+        pairs[pairAddress] = {
+          token0Address: `0x${log.topics[1].slice(26)}`,
+          token1Address: `0x${log.topics[2].slice(26)}`,
+        };
+
+        return pairAddress;
+      })
+      .filter((address) => !pairExist[address]),
+  );
+
+  await basicUtil.saveIntoCache(
+    {
+      block,
+      pairs: pairAddresses,
+      token01: pairs,
+    },
+    'cache/v3Pairs.json',
+    chain,
+    provider,
+  );
+
+  const tokens0 = await util.executeCallOfMultiTargets(
+    pairAddresses,
+    PAIR_ABI,
+    'token0',
+    [],
+    block,
+    chain,
+    web3,
+  );
+  const tokens1 = await util.executeCallOfMultiTargets(
+    pairAddresses,
+    PAIR_ABI,
+    'token1',
+    [],
+    block,
+    chain,
+    web3,
+  );
+
+  const token0Balances = await util.getTokenBalancesOfHolders(
+    pairAddresses,
+    tokens0.filter(Boolean),
+    block,
+    chain,
+    web3,
+  );
+
+  const token1Balances = await util.getTokenBalancesOfHolders(
+    pairAddresses,
+    tokens1.filter(Boolean),
+    block,
+    chain,
+    web3,
+  );
+
+  formatter.sumMultiBalanceOf(balances, token1Balances);
+  formatter.sumMultiBalanceOf(balances, token0Balances);
+  formatter.convertBalancesToFixed(balances);
+
+  return balances;
+}
+
+export default {
+  getTvl,
+};
