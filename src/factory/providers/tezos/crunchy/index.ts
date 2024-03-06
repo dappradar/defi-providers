e6f478d462b85a7f2eab48ae9bc3fb1d1cbf06cb
osman sonmezturk <osmansonmezturk@gmail.com>
2024-03-06 12:58:54 +0300

    taquito was updated and PRs was integrated

--- src/factory/providers/tezos/crunchy/index.ts
+++ src/factory/providers/tezos/crunchy/index.ts
@@ -4,7 +4,33 @@ import formatter from '../../../../util/formatter';
 import basicUtil from '../../../../util/basicUtil';
 import util from '../../../../util/blockchainUtil';
 
-const FARM_ADDRESS = 'KT1KnuE87q1EKjPozJ5sRAjQA24FPsP57CE3';
+const DEEP_FREEZERS_ADDRESS = 'KT1LjcQ4h5hCy9RcveFz9Pq8LtmF6oun7vNd';
+
+const FARM_ADDRESSES = [
+  'KT1KnuE87q1EKjPozJ5sRAjQA24FPsP57CE3',
+  'KT1L1WZgdsfjEyP5T4ZCYVvN5vgzrNbu18kX',
+];
+
+const WTZ_ADDRESSES = [
+  'KT1BB3oNr5vUSw1CuPNb2zpYEVp376XrXWaJ',
+  'KT1H25LW5k4HQGm9hmNXzaxf3nqjsAEhQPah',
+  'KT1LpGZnT6dj6STSxHXmvSPqx39ZdPXAMpFz',
+  'KT1NBgqqJacbdoeNAg9MvrgPT9h6q6AGWvFA',
+  'KT1NGTDBKDPMrAYEufb72CLwuQJ7jU7jL6jD',
+];
+
+async function getTezosBalance(address, block, web3) {
+  try {
+    const tezosBalance = await web3.eth.getBalance(address, block);
+
+    return {
+      token: 'xtz',
+      balance: BigNumber(tezosBalance),
+    };
+  } catch {}
+  return null;
+}
+
 let pools = {};
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
@@ -17,36 +43,89 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
     pools = await basicUtil.readFromCache('cache/pools.json', chain, provider);
   } catch {}
 
-  const contract = new web3.eth.Contract(null, FARM_ADDRESS);
-  await contract.init();
-  const farmLength = await contract.methods.nextFarmId().call(null, block);
-  const farms = await contract.methods.getBigmap('farms').call(null, block);
-
-  console.log(`farm length: ${farmLength}`);
-
   const balanceResults = [];
+  let globalIndex = 0;
 
-  for (let i = 0; i < farmLength; i++) {
+  for (let i = 0; i < FARM_ADDRESSES.length; i++) {
+    const contract = new web3.eth.Contract(null, FARM_ADDRESSES[i]);
+    await contract.init();
+
+    const farms = await contract.methods.getBigmap('farms').call(null, block);
+    const totalFarms = farms.map((farm) => farm.key);
+    const farmLength = totalFarms.length;
+
+    console.log(`Farm length for address ${FARM_ADDRESSES[i]}: ${farmLength}`);
     try {
-      if (!pools[i]) {
+      for (let j = 0; j < farmLength; j++) {
+        const farmIndex = j + i * globalIndex;
+
+        if (!pools[farmIndex]) {
+          let tokenId;
+          try {
+            const tokenType = Object.keys(farms[j].value.poolToken.tokenType);
+            if (tokenType[0] == 'fa2') {
+              tokenId = farms[j].value.poolToken.tokenId;
+            }
+          } catch {}
+          pools[farmIndex] = tokenId
+            ? `${farms[j].value.poolToken.address}_${tokenId}`
+            : farms[j].value.poolToken.address;
+        }
+
+        balanceResults.push({
+          token: pools[farmIndex],
+          balance: BigNumber(farms[j].value.poolBalance),
+        });
+      }
+      globalIndex += farmLength;
+    } catch {}
+  }
+
+  // Fetch WTZ Liquid Staking
+  for (let i = 0; i < WTZ_ADDRESSES.length; i++) {
+    const wtzIndex = i + globalIndex;
+    pools[wtzIndex] = WTZ_ADDRESSES[i];
+    const wtz = await getTezosBalance(WTZ_ADDRESSES[i], block, web3);
+    balanceResults.push(wtz);
+  }
+  globalIndex += WTZ_ADDRESSES.length;
+
+  // Fetch Deep Freezers TVL
+  const contract = new web3.eth.Contract(null, DEEP_FREEZERS_ADDRESS);
+  await contract.init();
+
+  const locks = await contract.methods.getBigmap('locks').call(null, block);
+  const totalLocks = locks.map((lock) => lock.key);
+  const locksLength = totalLocks.length;
+
+  console.log(
+    `Lock length for address $DEEP_FREEZERS_ADDRESS}: ${locksLength}`,
+  );
+  try {
+    for (let i = 0; i < locksLength; i++) {
+      const lockIndex = i + globalIndex;
+
+      if (!pools[lockIndex]) {
         let tokenId;
         try {
-          const tokenType = Object.keys(farms[i].value.poolToken.tokenType);
+          const tokenType = Object.keys(locks[i].value.token.tokenType);
           if (tokenType[0] == 'fa2') {
-            tokenId = farms[i].value.poolToken.tokenId;
+            tokenId = locks[i].value.token.tokenId;
           }
         } catch {}
-        pools[i] = tokenId
-          ? `${farms[i].value.poolToken.address}_${tokenId}`
-          : farms[i].value.poolToken.address;
+        pools[lockIndex] = tokenId
+          ? `${locks[i].value.token.address}_${tokenId}`
+          : locks[i].value.token.address;
       }
 
       balanceResults.push({
-        token: pools[i],
-        balance: BigNumber(farms[i].value.poolBalance),
+        token: pools[lockIndex],
+        balance: BigNumber(locks[i].value.amountLocked),
       });
-    } catch {}
-  }
+    }
+    globalIndex += Number(locksLength);
+  } catch {}
+
   await basicUtil.saveIntoCache(pools, 'cache/pools.json', chain, provider);
 
   const tokenBalances = {};
