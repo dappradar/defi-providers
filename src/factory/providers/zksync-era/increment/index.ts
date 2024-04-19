b7f9e823077c27c769148c14b90a375c068869bd
webthethird <ratzbodell@gmail.com>
2024-04-19 12:19:29 -0400

    Remove Collateral type def

--- src/factory/providers/zksync-era/increment/index.ts
+++ src/factory/providers/zksync-era/increment/index.ts
@@ -7,13 +7,6 @@ import VAULT_ABI from './abi.json';
 const START_BLOCK = 28541484;
 const VAULT_ADDRESS = '0x703500cAF3c79aF68BB3dc85A6846d1C7999C672';
 const USDC = '0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4';
-type Collateral = {
-  asset: string;
-  weight: BigInt;
-  decimals: number;
-  currentAmount: BigInt;
-  maxAmount: BigInt;
-};
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   const { block, chain, provider, web3 } = params;
@@ -21,7 +14,7 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
     return {};
   }
 
-  let collaterals: Collateral[] = [];
+  let collaterals = [];
   try {
     collaterals = await basicUtil.readFromCache(
       'cache/collaterals.json',
