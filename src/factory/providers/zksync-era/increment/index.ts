1834116fff7b971ce6c71dfc447663d2feb21e37
omurovec <owenmurovec@gmail.com>
2024-04-19 17:16:22 +0200

    add multiple collateral types

--- src/factory/providers/zksync-era/increment/index.ts
+++ src/factory/providers/zksync-era/increment/index.ts
@@ -1,37 +1,11 @@
-import BigNumber from 'bignumber.js';
-import { request, gql } from 'graphql-request';
-import basicUtil from '../../../../util/basicUtil';
+import util from '../../../../util/blockchainUtil';
 import formatter from '../../../../util/formatter';
 import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+import VAULT_ABI from './abi.json';
 
 const START_BLOCK = 28541484;
 const VAULT_ADDRESS = '0x703500cAF3c79aF68BB3dc85A6846d1C7999C672';
-const SUBGRAPH_ENDPOINT =
-  'https://subgraph.increment.finance/subgraphs/name/Increment-Finance/subgraph';
-const TVL_QUERY = gql`
-  query Tvl($timestamp: Int!) {
-    tvlHourlyAggregates(
-      where: { openTimestamp_lte: $timestamp, closeTimestamp_gt: $timestamp }
-    ) {
-      openTimestamp
-      closeTimestamp
-      amount
-    }
-  }
-`;
-
-async function getTVL(block, chain, web3) {
-  let amount;
-  const { timestamp } = await web3.eth.getBlock(block);
-  try {
-    amount = await request(SUBGRAPH_ENDPOINT, TVL_QUERY, {
-      timestamp,
-    }).then((data) => data.tvlHourlyAggregates[0].amount);
-  } catch {
-    amount = await getTVL(block - basicUtil.getDelay(chain), chain, web3);
-  }
-  return amount;
-}
+const USDC = '0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4';
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   const { block, chain, web3 } = params;
@@ -39,10 +13,40 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
     return {};
   }
 
-  const balances = {};
-  const amount = await getTVL(block, chain, web3);
-  balances[VAULT_ADDRESS] = BigNumber(amount);
+  const numCollaterals = await util.executeCall(
+    VAULT_ADDRESS,
+    VAULT_ABI,
+    'getNumberOfCollaterals',
+    [],
+    block,
+    chain,
+    web3,
+  );
 
+  const collaterals = await util.executeMultiCallsOfTarget(
+    VAULT_ADDRESS,
+    VAULT_ABI,
+    'getWhitelistedCollateral',
+    Array.from({ length: numCollaterals }, (v, i) => [i]),
+    block,
+    chain,
+    web3,
+  );
+
+  const balanceResults = await util.getTokenBalances(
+    VAULT_ADDRESS,
+    collaterals.map((collateral) => collateral.asset),
+    block,
+    chain,
+    web3,
+  );
+
+  // replace UA with USDC address
+  const ua = collaterals.shift();
+  collaterals.unshift({ token: USDC, balance: ua.balance });
+
+  const balances = {};
+  formatter.sumMultiBalanceOf(balances, balanceResults);
   formatter.convertBalancesToFixed(balances);
   return { balances };
 }
