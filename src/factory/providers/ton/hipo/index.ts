d8dd297acf02914c8a16d678704a0d6f37aabcd3
Mantas S <mantas487@gmail.com>
2024-07-03 17:01:56 +0300

    TON bemo, hipo integrations

---
+++ src/factory/providers/ton/hipo/index.ts
@@ -0,0 +1,20 @@
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+import formatter from '../../../../util/formatter';
+
+const CONTRACT_ADDRESS = 'EQCLyZHP4Xe8fpchQz76O-_RmUhaVc_9BAoGyJrwJrcbz2eZ';
+const METHOD = 'get_treasury_state';
+const TON_ADDRESS = 'EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c';
+
+async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
+  const { block, chain, provider, web3 } = params;
+
+  const balances = {};
+  const response = await web3.eth.call(CONTRACT_ADDRESS, METHOD);
+
+  balances[TON_ADDRESS] = response[0];
+
+  formatter.convertBalancesToFixed(balances);
+  return { balances };
+}
+
+export { tvl };
