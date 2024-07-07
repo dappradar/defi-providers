8824bcd25d70906b52241bcbcead64236f25d268
Mantas S <mantas487@gmail.com>
2024-07-07 13:03:20 +0300

    Update index.ts

--- src/factory/providers/ton/stormtrade/index.ts
+++ src/factory/providers/ton/stormtrade/index.ts
@@ -2,10 +2,10 @@ import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 import formatter from '../../../../util/formatter';
 
 const CONTRACT_ADDRESSES = [
-  'EQDynReiCeK8xlKRbYArpp4jyzZuF6-tYfhFM0O5ulOs5H0L', // jUSDT vault
-  'EQDpJnZP89Jyxz3euDaXXFUhwCWtaOeRmiUJTi3jGYgF8fnj', // TON-M vault
-  'EQAz6ehNfL7_8NI7OVh1Qg46HsuC4kFpK-icfqK9J3Frd6CJ', // USDT vault
-  'EQBwfRtqEf3ZzhkeGsmXiC7hzTh1C5zZZzLgDH5VL8gENQ2A', // Notcoin pre-market vault
+  'EQDynReiCeK8xlKRbYArpp4jyzZuF6-tYfhFM0O5ulOs5H0L',
+  'EQDpJnZP89Jyxz3euDaXXFUhwCWtaOeRmiUJTi3jGYgF8fnj',
+  'EQAz6ehNfL7_8NI7OVh1Qg46HsuC4kFpK-icfqK9J3Frd6CJ',
+  'EQBwfRtqEf3ZzhkeGsmXiC7hzTh1C5zZZzLgDH5VL8gENQ2A',
 ];
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
