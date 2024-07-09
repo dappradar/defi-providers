8d7e952a30d5209c7f0f829e2e616094cde725dc
Mantas S <mantas487@gmail.com>
2024-07-09 16:16:40 +0300

    3 integrations

---
+++ src/factory/providers/bahamut/silkswap/index.ts
@@ -0,0 +1,28 @@
+import formatter from '../../../../util/formatter';
+import uniswapV2 from '../../../../util/calculators/uniswapV2';
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+
+const START_BLOCK = 2388831;
+const FACTORY_ADDRESS = '0xd0c5d23290d63e06a0c4b87f14bd2f7aa551a895';
+
+async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
+  const { block, chain, provider, web3 } = params;
+
+  if (block < START_BLOCK) {
+    return {};
+  }
+
+  const { balances, poolBalances } = await uniswapV2.getTvl(
+    FACTORY_ADDRESS,
+    block,
+    chain,
+    provider,
+    web3,
+  );
+
+  formatter.convertBalancesToFixed(balances);
+
+  return { balances, poolBalances };
+}
+
+export { tvl };
