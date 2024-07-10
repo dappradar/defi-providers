e8ca0970d5cc568628d600b69cf51154ca4f82ac
Mantas S <mantas487@gmail.com>
2024-07-10 14:14:22 +0300

    Update index.ts

--- src/factory/providers/ethereum/uniswap/index.ts
+++ src/factory/providers/ethereum/uniswap/index.ts
@@ -5,8 +5,9 @@ import formatter from '../../../../util/formatter';
 
 const START_BLOCK = 10000835;
 const QUERY_SIZE = 1000;
-const SUBGRAPH_ENDPOINT =
-  'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v2-dev';
+const THE_GRAPH_API_KEY = process.env?.THE_GRAPH_API_KEY;
+
+const SUBGRAPH_ENDPOINT = `https://gateway-arbitrum.network.thegraph.com/api/${THE_GRAPH_API_KEY}/subgraphs/id/EYCKATKGBKLWvSfwvBjzfCBmGwYNdVkduYXVivCsLRFu`;
 const TOKENS = gql`
   query getTokens($id: String!, $block: Int!) {
     tokens(
@@ -24,7 +25,6 @@ const TOKENS = gql`
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   const { block, chain, provider, web3 } = params;
-
   if (block < START_BLOCK) {
     return {};
   }
@@ -33,7 +33,7 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   let lastId = '';
   while (true) {
     const requestResult = await request(SUBGRAPH_ENDPOINT, TOKENS, {
-      block: block,
+      block: block - 100,
       id: lastId,
     });
 
