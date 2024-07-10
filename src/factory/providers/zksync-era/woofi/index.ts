500224e9e9eced3bdd9d9c0ba3008b25e4af2b89
Mantas S <mantas487@gmail.com>
2024-07-10 17:25:38 +0300

    base balancer, zksync-era woofi

--- src/factory/providers/zksync-era/woofi/index.ts
+++ src/factory/providers/zksync-era/woofi/index.ts
@@ -4,7 +4,7 @@ import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 import basicUtil from '../../../../util/basicUtil';
 
 const START_BLOCK = 1466617;
-const WOOPPV2_ADDRESS = '0x42ED123EB5266A5B8E2B54B2C76180CCF5e72FEe';
+const WOOPPV2_ADDRESS = '0xE656d70bc3550e3EEE9dE7dC79367A44Fd13d975';
 const USDC_ADDRESS = '0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4';
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
@@ -16,7 +16,11 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
 
   const tokenBalances = await util.getTokenBalances(
     WOOPPV2_ADDRESS,
-    [USDC_ADDRESS, basicUtil.getWmainAddress(chain)],
+    [
+      USDC_ADDRESS,
+      basicUtil.getWmainAddress(chain),
+      '0x1d17CBcF0D6D143135aE902365D2E5e2A16538D4',
+    ],
     block,
     chain,
     web3,
