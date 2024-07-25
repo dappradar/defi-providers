e0e5468ee5fb533850e375e32a00a14c46c3e3d3
Mantas S <mantas487@gmail.com>
2024-07-25 15:59:05 +0300

    fantom

--- src/factory/providers/fantom/sushiswap/index.ts
+++ src/factory/providers/fantom/sushiswap/index.ts
@@ -1,14 +1,9 @@
-import BigNumber from 'bignumber.js';
-import { kashiLending } from './kashi-lending';
 import formatter from '../../../../util/formatter';
 import uniswapV2 from '../../../../util/calculators/uniswapV2';
 import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 
 const START_BLOCK = 2457879;
 const FACTORY_ADDRESS = '0xc35DADB65012eC5796536bD9864eD8773aBc74C4';
-const GRAPHQL_API =
-  'https://api.thegraph.com/subgraphs/name/sushiswap/fantom-exchange';
-const QUERY_SIZE = 400;
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   const { block, chain, provider, web3 } = params;
@@ -17,7 +12,7 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
     return {};
   }
 
-  const { balances: dexBalances, poolBalances } = await uniswapV2.getTvl(
+  const { balances, poolBalances } = await uniswapV2.getTvl(
     FACTORY_ADDRESS,
     block,
     chain,
@@ -25,41 +20,9 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
     web3,
   );
 
-  //const kashiBalances = await kashiLending(block, chain, web3);
-
-  const balances = formatter.sum([dexBalances /*, kashiBalances*/]);
-
-  for (const token in balances) {
-    if (BigNumber(balances[token] || 0).isLessThan(100000)) {
-      delete balances[token];
-    }
-  }
+  formatter.convertBalancesToFixed(balances);
 
   return { balances, poolBalances };
 }
 
-async function getPoolVolumes(pools, priorBlockNumber) {
-  const poolVolumes = await uniswapV2.getPoolVolumes(
-    GRAPHQL_API,
-    QUERY_SIZE,
-    pools,
-    priorBlockNumber,
-    null,
-  );
-
-  return poolVolumes;
-}
-
-async function getTokenVolumes(tokens, priorBlockNumber) {
-  const tokenVolumes = await uniswapV2.getTokenVolumes(
-    GRAPHQL_API,
-    QUERY_SIZE,
-    tokens,
-    priorBlockNumber,
-    null,
-  );
-
-  return tokenVolumes;
-}
-
-export { tvl, getPoolVolumes, getTokenVolumes };
+export { tvl };
