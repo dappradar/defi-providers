3113bbf836151c307c97e6eb01b47f5fa941130d
Mantas S <mantas487@gmail.com>
2024-07-25 16:12:37 +0300

    sushiswap integrations

---
+++ src/factory/providers/core/sushiswap/index.ts
@@ -0,0 +1,28 @@
+import formatter from '../../../../util/formatter';
+import uniswapV2 from '../../../../util/calculators/uniswapV2';
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+
+const START_BLOCK = 8051339;
+const FACTORY_ADDRESS = '0xB45e53277a7e0F1D35f2a77160e91e25507f1763';
+
+async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
+  const { block, chain, provider, web3 } = params;
+
+  if (block < START_BLOCK) {
+    return {};
+  }
+
+  const { balances, poolBalances } = await uniswapV2.getTvl(
+    FACTORY_ADDRESS,
+    block,
+    chain,
+    provider,
+    web3,
+  );
+
+  formatter.convertBalancesToFixed(balances);
+
+  return { balances, poolBalances };
+}
+
+export { tvl };
