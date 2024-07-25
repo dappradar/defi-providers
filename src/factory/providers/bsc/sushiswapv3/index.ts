257eab012850de5c78a036049a91b3db95c3bdaf
Mantas S <mantas487@gmail.com>
2024-07-25 15:23:50 +0300

    Create index.ts

---
+++ src/factory/providers/bsc/sushiswapv3/index.ts
@@ -0,0 +1,25 @@
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+import uniswapV3 from '../../../../util/calculators/uniswapV3chain';
+
+const V3_START_BLOCK = 26976538;
+const V3_FACTORY_ADDRESS = '0x126555dd55a39328F69400d6aE4F782Bd4C34ABb';
+
+async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
+  const { block, chain, provider, web3 } = params;
+  if (block < V3_START_BLOCK) {
+    return { balances: {} };
+  }
+
+  const balances = await uniswapV3.getTvl(
+    V3_FACTORY_ADDRESS,
+    V3_START_BLOCK,
+    block,
+    chain,
+    provider,
+    web3,
+  );
+
+  return { balances, poolBalances: {} };
+}
+
+export { tvl };
