c4c3b9129f4c04697fff0710a2e5c72ca63744b9
Mantas S <mantas487@gmail.com>
2024-09-03 16:15:07 +0300

    Update index.ts

--- src/factory/providers/aurora/synapse/index.ts
+++ src/factory/providers/aurora/synapse/index.ts
@@ -2,6 +2,7 @@ import util from '../../../../util/blockchainUtil';
 import formatter from '../../../../util/formatter';
 import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 
+const START_BLOCK = 56525135;
 const POOL = '0xcEf6C2e20898C2604886b888552CA6CcF66933B0';
 const TOKENS = [
   '0x4988a896b1227218e4A686fdE5EabdcAbd91571f',
@@ -11,8 +12,8 @@ const TOKENS = [
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   const { block, chain, provider, web3 } = params;
 
-  if (block < 56525135) {
-    return {};
+  if (block < START_BLOCK) {
+    return { balances: {} };
   }
 
   const tokenBalances = await util.getTokenBalances(
