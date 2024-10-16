091e3a980d0b3286989324f80b52600f4d659038
zxselimcan <zxselimcan@icloud.com>
2024-10-16 10:10:07 +0300

    blast squadswap integration

---
+++ src/factory/providers/blast/squadswap/index.ts
@@ -0,0 +1,25 @@
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+import uniswapV2 from '../../../../util/calculators/uniswapV2';
+import formatter from '../../../../util/formatter';
+
+const START_BLOCK = 5345079;
+const FACTORY_ADDRESS = '0x4B599f3425D54AfBf94bFD41EA9931fF92AD6551';
+
+async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
+  const { block, chain, provider, web3 } = params;
+  if (block < START_BLOCK) {
+    return {};
+  }
+
+  const { balances, poolBalances } = await uniswapV2.getTvl(
+    FACTORY_ADDRESS,
+    block,
+    chain,
+    provider,
+    web3,
+  );
+  formatter.convertBalancesToFixed(balances);
+  return { balances, poolBalances };
+}
+
+export { tvl };
