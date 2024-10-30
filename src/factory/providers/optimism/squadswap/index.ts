a88c0bad7696de65cb75ffd440e6e5d768af767e
Mantas S <mantas487@gmail.com>
2024-10-30 16:12:59 +0200

    optimism, polygon

--- src/factory/providers/optimism/squadswap/index.ts
+++ src/factory/providers/optimism/squadswap/index.ts
@@ -1,25 +1,43 @@
-import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
-import uniswapV2 from '../../../../util/calculators/uniswapV2';
 import formatter from '../../../../util/formatter';
+import uniswapV2 from '../../../../util/calculators/uniswapV2';
+import uniswapV3 from '../../../../util/calculators/uniswapV3chain';
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 
 const START_BLOCK = 125322305;
-const FACTORY_ADDRESS = '0xba34aA640b8Be02A439221BCbea1f48c1035EEF9';
+const V3_START_BLOCK = 125326692;
+const V2_FACTORY_ADDRESS = '0xba34aA640b8Be02A439221BCbea1f48c1035EEF9';
+const V3_FACTORY_ADDRESS = '0xa1288b64F2378276d0Cc56F08397F70BecF7c0EA';
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   const { block, chain, provider, web3 } = params;
+  let balances = {};
   if (block < START_BLOCK) {
-    return {};
+    return { balances };
   }
 
-  const { balances, poolBalances } = await uniswapV2.getTvl(
-    FACTORY_ADDRESS,
+  const v2 = await uniswapV2.getTvl(
+    V2_FACTORY_ADDRESS,
     block,
     chain,
     provider,
     web3,
   );
-  formatter.convertBalancesToFixed(balances);
-  return { balances, poolBalances };
+
+  if (block >= V3_START_BLOCK) {
+    balances = await uniswapV3.getTvl(
+      V3_FACTORY_ADDRESS,
+      V3_START_BLOCK,
+      block,
+      chain,
+      provider,
+      web3,
+      'algebra',
+    );
+  }
+
+  balances = formatter.sum([v2.balances, balances]);
+
+  return { balances };
 }
 
 export { tvl };
--- src/factory/providers/polygon/squadswap/index.ts
+++ src/factory/providers/polygon/squadswap/index.ts
@@ -1,25 +1,43 @@
-import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
-import uniswapV2 from '../../../../util/calculators/uniswapV2';
 import formatter from '../../../../util/formatter';
+import uniswapV2 from '../../../../util/calculators/uniswapV2';
+import uniswapV3 from '../../../../util/calculators/uniswapV3chain';
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 
 const START_BLOCK = 61787381;
-const FACTORY_ADDRESS = '0xEE8F37D490CB7Ea1dae7d080c5738894731299f0';
+const V3_START_BLOCK = 61864971;
+const V2_FACTORY_ADDRESS = '0xEE8F37D490CB7Ea1dae7d080c5738894731299f0';
+const V3_FACTORY_ADDRESS = '0x633Faf3DAc3677b51ea7A53a81b79AEe944714dc';
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   const { block, chain, provider, web3 } = params;
+  let balances = {};
   if (block < START_BLOCK) {
-    return {};
+    return { balances };
   }
 
-  const { balances, poolBalances } = await uniswapV2.getTvl(
-    FACTORY_ADDRESS,
+  const v2 = await uniswapV2.getTvl(
+    V2_FACTORY_ADDRESS,
     block,
     chain,
     provider,
     web3,
   );
-  formatter.convertBalancesToFixed(balances);
-  return { balances, poolBalances };
+
+  if (block >= V3_START_BLOCK) {
+    balances = await uniswapV3.getTvl(
+      V3_FACTORY_ADDRESS,
+      V3_START_BLOCK,
+      block,
+      chain,
+      provider,
+      web3,
+      'algebra',
+    );
+  }
+
+  balances = formatter.sum([v2.balances, balances]);
+
+  return { balances };
 }
 
 export { tvl };
