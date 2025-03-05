60310b9cf739a4d5a2ef00aa02284ff4e359b008
Mantas S <mantas487@gmail.com>
2025-03-05 15:13:22 +0200

    Create index.ts

---
+++ src/factory/providers/boba-eth/icecreamswap/index.ts
@@ -0,0 +1,27 @@
+import uniswapV2 from '../../../../util/calculators/uniswapV2';
+import formatter from '../../../../util/formatter';
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+
+const START_BLOCK = 1441895;
+const FACTORY_ADDRESS = '0x39135bD1FD3A04381C1094AB0Dd35c7bbe61b3C5';
+
+async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
+  const { block, chain, provider, web3 } = params;
+  if (block < START_BLOCK) {
+    return { balances: {} };
+  }
+
+  const { balances, poolBalances } = await uniswapV2.getTvl(
+    FACTORY_ADDRESS,
+    block,
+    chain,
+    provider,
+    web3,
+  );
+
+  formatter.convertBalancesToFixed(balances);
+
+  return { balances, poolBalances };
+}
+
+export { tvl };
