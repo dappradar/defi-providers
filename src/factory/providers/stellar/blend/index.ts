bb8bc2a81130decbd25b700f427ac9c3e51fc818
Mantas S <mantas487@gmail.com>
2025-05-13 21:02:41 +0300

    Update index.ts

--- src/factory/providers/stellar/blend/index.ts
+++ src/factory/providers/stellar/blend/index.ts
@@ -15,7 +15,6 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   const balances = {};
 
   const tokenBalances = await web3.eth.getAddressesBalances(HOLDERS);
-  console.log('tokenBalances', tokenBalances);
 
   formatter.sumMultiBalanceOf(balances, tokenBalances);
   formatter.mapStellarTokenAddresses(balances);
