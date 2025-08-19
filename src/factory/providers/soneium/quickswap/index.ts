645f18393b3ea96848274f036f698d75a2a7de5b
Mantas S <mantas487@gmail.com>
2025-08-19 16:45:12 +0300

    soneium

---
+++ src/factory/providers/soneium/quickswap/index.ts
@@ -0,0 +1,26 @@
+import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
+import uniswapV3 from '../../../../util/calculators/uniswapV3chain';
+
+const V3_START_BLOCK = 1681559;
+const V3_FACTORY_ADDRESS = '0x8Ff309F68F6Caf77a78E9C20d2Af7Ed4bE2D7093';
+
+async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
+  const { block, chain, provider, web3 } = params;
+  if (block < V3_START_BLOCK) {
+    return { balances: {} };
+  }
+
+  const balances = await uniswapV3.getTvl(
+    V3_FACTORY_ADDRESS,
+    V3_START_BLOCK,
+    block,
+    chain,
+    provider,
+    web3,
+    'algebra',
+  );
+
+  return { balances, poolBalances: {} };
+}
+
+export { tvl };
