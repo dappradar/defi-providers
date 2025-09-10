b5c5c7da66f213dfb1d330593e3bd20273fbad62
Mantas S <mantas487@gmail.com>
2025-09-10 12:55:15 +0300

    fixes

--- src/factory/providers/injective/dojoswap/index.ts
+++ src/factory/providers/injective/dojoswap/index.ts
@@ -1,47 +1,17 @@
 import formatter from '../../../../util/formatter';
 import { ITvlParams, ITvlReturn } from '../../../../interfaces/ITvl';
 import BigNumber from 'bignumber.js';
-import fetch from 'node-fetch';
 import cosmosDex from '../../../../util/calculators/cosmosDex';
 
 const DINJ_ADDRESS = 'inj17glv5mk2pvhpwkdjljacmr2fx9pfc3djepy6xh';
 const INJ_ADDRESS = 'inj';
 const STATE_METHOD = 'state';
-const POOLS_API_URL = 'https://analytics.dojo.trading/dashboard/pools/tvl';
 const FACTORY_ADDRESS = 'inj1pc2vxcmnyzawnwkf03n2ggvt997avtuwagqngk';
-
-interface Pool {
-  stakingAddress: string;
-  vault: {
-    config: {
-      staking_token: string;
-    };
-  };
-}
-
-async function getPoolBalances(pools: Pool[], web3) {
-  let balances = {};
-
-  for (const pool of Object.values(pools)) {
-    const MARKET_ADDRESS = pool.stakingAddress;
-    const CW20_TOKEN_ADDRESSES = [pool.vault.config.staking_token];
-
-    const marketCw20TokenBalances = await web3.eth.getAccountCw20TokenBalances(
-      MARKET_ADDRESS,
-      CW20_TOKEN_ADDRESSES,
-    );
-
-    balances = formatter.sum([balances, marketCw20TokenBalances]);
-  }
-
-  return balances;
-}
 
 async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   const { block, chain, provider, web3 } = params;
 
-  const pools = await fetch(POOLS_API_URL).then((res) => res.json());
-  let balances = await getPoolBalances(pools, web3);
+  let balances = {};
 
   balances[INJ_ADDRESS] = BigNumber(balances[INJ_ADDRESS] || 0).plus(
     await web3.eth
@@ -57,6 +27,13 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
     web3,
   );
   balances = formatter.sum([balances, dexBalances]);
+
+  if (balances['inj18luqttqyckgpddndh8hvaq25d5nfwjc78m56lc']) {
+    balances['inj'] = BigNumber(balances['inj'] || 0)
+      .plus(balances['inj18luqttqyckgpddndh8hvaq25d5nfwjc78m56lc'])
+      .toFixed();
+    delete balances['inj18luqttqyckgpddndh8hvaq25d5nfwjc78m56lc'];
+  }
 
   formatter.convertBalancesToFixed(balances);
   return { balances };
--- src/factory/providers/injective/wormhole/index.ts
+++ src/factory/providers/injective/wormhole/index.ts
@@ -10,6 +10,13 @@ async function tvl(params: ITvlParams): Promise<Partial<ITvlReturn>> {
   const portalBalances = await web3.eth.getAccountBalances(PORTAL_ADDRESS);
   balances = formatter.sum([balances, portalBalances]);
 
+  if (balances['inj18luqttqyckgpddndh8hvaq25d5nfwjc78m56lc']) {
+    balances['inj'] = BigNumber(balances['inj'] || 0)
+      .plus(balances['inj18luqttqyckgpddndh8hvaq25d5nfwjc78m56lc'])
+      .toFixed();
+    delete balances['inj18luqttqyckgpddndh8hvaq25d5nfwjc78m56lc'];
+  }
+
   formatter.convertBalancesToFixed(balances);
   return { balances };
 }
