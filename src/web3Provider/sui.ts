aa293ee2f075a1ab6a813fc70f690d9691c8487c
Mantas S <mantas487@gmail.com>
2025-09-30 14:02:38 +0300

    suilend

--- src/web3Provider/sui.ts
+++ src/web3Provider/sui.ts
@@ -33,4 +33,71 @@ export class Sui {
     });
     return (result.data.content as any).fields;
   }
+
+  async queryEvents({ eventType, transform = (i) => i }) {
+    let filter: any = {};
+    if (eventType) {
+      filter = { MoveEventType: eventType };
+    }
+
+    const items = [];
+    let cursor = null;
+    let hasNextPage = true;
+
+    while (hasNextPage) {
+      const result = await this.client.queryEvents({
+        query: filter,
+        cursor,
+        limit: 50,
+        order: 'ascending',
+      });
+
+      items.push(...result.data);
+      cursor = result.nextCursor;
+      hasNextPage = result.hasNextPage;
+    }
+
+    return items.map((i) => i.parsedJson).map(transform);
+  }
+
+  async getDynamicFieldObjects({ parent, cursor = null, limit = 50 }) {
+    const items = [];
+    let nextCursor = cursor;
+    let hasNextPage = true;
+
+    while (hasNextPage) {
+      const result = await this.client.getDynamicFields({
+        parentId: parent,
+        cursor: nextCursor,
+        limit,
+      });
+
+      for (const field of result.data) {
+        try {
+          const obj = await this.client.getObject({
+            id: field.objectId,
+            options: {
+              showType: true,
+              showOwner: true,
+              showContent: true,
+            },
+          });
+          items.push(obj.data.content);
+        } catch (error) {
+          continue;
+        }
+      }
+      nextCursor = result.nextCursor;
+      hasNextPage = result.hasNextPage;
+    }
+
+    return items;
+  }
+
+  async devInspectTransactionBlock(sender: string, txBytes: string) {
+    return await this.client.devInspectTransactionBlock({
+      sender,
+      transactionBlock: txBytes,
+    });
+  }
 }
