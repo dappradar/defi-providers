e29edc3d61db1686349a89f8ff95fd90bf0dfcff
Mantas S <mantas487@gmail.com>
2025-10-02 09:44:32 +0300

    Update index.ts

--- src/factory/providers/sui/cetus/index.ts
+++ src/factory/providers/sui/cetus/index.ts
@@ -17,14 +17,29 @@ async function getStakingBalances(
 
   const xCetusManagerInfo = await web3.getObject(XCETUS_MANAGER_ID);
 
+  log.info({
+    message: 'Processing Cetus staking balances',
+    endpoint: 'getStakingBalances',
+  });
+
   if (xCetusManagerInfo?.treasury?.fields?.total_supply?.fields?.value) {
     const cetusAmount = new BigNumber(
       xCetusManagerInfo.treasury.fields.total_supply.fields.value,
     );
 
+    log.info({
+      message: `Cetus staking amount: ${cetusAmount.toFixed(0)}`,
+      endpoint: 'getStakingBalances',
+    });
+
     if (cetusAmount.isGreaterThan(0)) {
       formatter.merge(balances, CETUS_TOKEN_ADDRESS, cetusAmount.toFixed(0));
     }
+  } else {
+    log.info({
+      message: 'No Cetus staking data found',
+      endpoint: 'getStakingBalances',
+    });
   }
 
   return balances;
@@ -43,17 +58,32 @@ async function getAmmBalances(web3: any): Promise<{ [key: string]: string }> {
   );
   const poolInfos = await web3.getObjects(poolIds);
 
+  log.info({
+    message: `Processing ${poolInfos.length} Cetus AMM pools`,
+    endpoint: 'getAmmBalances',
+  });
+
+  let poolsWithBalances = 0;
   poolInfos.forEach((poolInfo: any) => {
     const typeStr = poolInfo.type;
     const fields = poolInfo.fields;
     const [coinA, coinB] = typeStr.split('<')[1].replace('>', '').split(', ');
 
+    let hasBalance = false;
     if (fields.coin_a && new BigNumber(fields.coin_a).isGreaterThan(0)) {
       formatter.merge(balances, coinA, fields.coin_a);
+      hasBalance = true;
     }
     if (fields.coin_b && new BigNumber(fields.coin_b).isGreaterThan(0)) {
       formatter.merge(balances, coinB, fields.coin_b);
+      hasBalance = true;
     }
+    if (hasBalance) poolsWithBalances++;
+  });
+
+  log.info({
+    message: `Found balances in ${poolsWithBalances}/${poolInfos.length} pools`,
+    endpoint: 'getAmmBalances',
   });
   return balances;
 }
